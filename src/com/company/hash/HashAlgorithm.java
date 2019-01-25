781f54294e24e3b9eafe67d6071fa56bdca4d715
estelle <15638203409@163.com>
2019-01-25 09:46:40 +0800

    代码格式化   by estelle0924

--- src/com/company/hash/HashAlgorithm.java
+++ src/com/company/hash/HashAlgorithm.java
@@ -15,8 +15,8 @@ import java.util.Vector;
  */
 public class HashAlgorithm {
     private static final int Num_training_user = 0;
-    protected ArrayList DataList;
     public Vector TestDataVector;
+    protected ArrayList DataList;
     protected Vector TrainDataVector;
 
     protected int Num_training_users;
@@ -30,80 +30,81 @@ public class HashAlgorithm {
     private int i;
     private Enumeration item_users_Key;
 
-    public  void readData(){
-        DataList =new ArrayList();
-        try{
-            FileReader f=new FileReader("H://ml-100k//u.data");
-            BufferedReader reader =new BufferedReader(f);
-            String line =null;
-            while((line=reader.readLine())!=null){
-                String item[] =line.split("");
-                String last = item(item.length-1);
+    public void readData() {
+        DataList = new ArrayList();
+        try {
+            FileReader f = new FileReader("H://ml-100k//u.data");
+            BufferedReader reader = new BufferedReader(f);
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                String item[] = line.split("");
+                String last = item(item.length - 1);
                 DataList.add(line);
             }
             System.out.println();
             System.out.println();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     private String item(int i) {
         // TODO Auto-generated method stub
         return null;
     }
 
 
-
-
     private String RunningInstance(int i) {
         // TODO Auto-generated method stub
         return null;
     }
-    public void bulidUsersItemsTable(){
-        Enumeration eles=TrainDataVector.elements();
+
+    public void bulidUsersItemsTable() {
+        Enumeration eles = TrainDataVector.elements();
         users_items = new Hashtable();
-        eles=TrainDataVector.elements();
-        while (eles.hasMoreElements()){
-            String ele =(String)eles.nextElement();
-            String[] RunningInstance =ele.split("   ");
+        eles = TrainDataVector.elements();
+        while (eles.hasMoreElements()) {
+            String ele = (String) eles.nextElement();
+            String[] RunningInstance = ele.split("   ");
             String user = RunningInstance[0];
             String item = RunningInstance[1];
         }
-        Object user=new Object();
-        if (users_items.containsKey(user)){
+        Object user = new Object();
+        if (users_items.containsKey(user)) {
             Vector items = new Vector();
-            items = (Vector)user_items.get(user);
+            items = (Vector) user_items.get(user);
             items.add(items);
             user_items.remove(user);
             user_items.get(items);
-        }else{
+        } else {
             Vector items = new Vector();
             items.add(user);
             user_items.put(user, items);
         }
     }
-    public void buildUsersItemsTable(){
-        Enumeration eles =TrainDataVector.elements();
+
+    public void buildUsersItemsTable() {
+        Enumeration eles = TrainDataVector.elements();
         items_users = new Hashtable();
-        eles=TrainDataVector.elements();
-        while (eles.hasMoreElements());
-        String els = (String)eles.nextElement();
+        eles = TrainDataVector.elements();
+        while (eles.hasMoreElements()) ;
+        String els = (String) eles.nextElement();
         String[] RunningInstance = els.split("    ");
         String user = RunningInstance[0];
-        String item =RunningInstance[1];
+        String item = RunningInstance[1];
 
-        if (items_users.containsKey(item)){
+        if (items_users.containsKey(item)) {
             Vector Items = new Vector();
-            item = (String)items_users.get(item);
-            ArrayList users=new ArrayList();
+            item = (String) items_users.get(item);
+            ArrayList users = new ArrayList();
             users.add(user);
             items_user.remove(user);
-            items_user.put(item,users);
-        }else{
+            items_user.put(item, users);
+        } else {
             Vector Items = new Vector();
             Items.add(user);
-            Object users=new Object();
-            user_items.put(item,users);
+            Object users = new Object();
+            user_items.put(item, users);
         }
     }
 
